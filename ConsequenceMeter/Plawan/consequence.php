817a9acaf0d75350324709c472ff60f91174605c
Plawan Kumar Rath <plawanrath@gmail.com>
2014-11-09 04:17:36 -0600

    Latest Upload
    With Algorithm implemented and drain rate generated. Need to implement
    javascript and front end.

---
+++ ConsequenceMeter/Plawan/consequence.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+$email = $_SESSION['Email'];
+$name = $_SESSION['Name'];
+$income = $_SESSION['Income'];
+$recIncome = $_SESSION['RecExpense'];
+if (isset($_POST['savings']) && !empty($_POST['savings']) && isset($_POST['miscExpenditure']) && !empty($_POST['miscExpenditure']) && (isset($_POST['itemToBuy'])) && !empty($_POST['itemToBuy']))
+{
+	$savings = $_POST['savings'];
+	$miscExp = $_POST['miscExpenditure'];
+	$item = $_POST['itemToBuy'];
+	$total_expense_expected = $miscExp + $recIncome;
+	$apppx_monthly_save = $income - $total_expense_expected;
+	$total_save_approx = $savings + $apppx_monthly_save;
+	$monthly_percent_save = ($apppx_monthly_save / $income) * 100;
+	if (isset($_POST['paymentMode']) && strcmp($_POST['paymentMode'],"savMode") == 0){
+			$new_savings = $savings - $item;
+			$new_approx_save = $new_savings + $apppx_monthly_save;
+			$drain = (($total_save_approx - $new_approx_save) / $total_save_approx) * 100;
+		}
+	elseif (isset($_POST['paymentMode']) && strcmp($_POST['paymentMode'],"emiMode") == 0){
+			$emi = $_POST['emi'];
+			$income1 = $income - $emi;
+			$new_save1 = ($monthly_percent_save / 100) * $income1;
+			$new_save_emi = $new_save1 + $savings;
+			$drain = (($total_save_approx - $new_save_emi) / $total_save_approx) * 100;
+		}	
+	else {
+			$emi = $_POST['emi'];
+			$dp = $_POST['downPayment'];
+			$new_save_left = $savings - $dp;
+			$income2 = $income - $emi;
+			$new_save2 = ($monthly_percent_save / 100) * $income2;
+			$new_save_buy = $new_save_left + $new_save2;
+			$drain = (($total_save_approx - $new_save_buy) / $total_save_approx) * 100;
+		}
+		if ($drain == 0)
+		{
+			$cm = 10;
+			}
+		if ($drain > 0 && $drain <= 10)
+		{
+			$cm = 9;
+			}
+		if ($drain > 10 && $drain <= 20)
+		{
+			$cm = 8;
+			}
+		if ($drain > 20 && $drain <= 30)
+		{
+			$cm = 7;
+			}
+		if ($drain > 30 && $drain <= 40)
+		{
+			$cm = 6;
+			}
+		if ($drain > 40 && $drain <= 50)
+		{
+			$cm = 5;
+			}
+		if ($drain > 50 && $drain <= 60)
+		{
+			$cm = 4;
+			}
+		if ($drain > 60 && $drain <= 70)
+		{
+			$cm = 3;
+			}
+		if ($drain > 70 && $drain <= 80)
+		{
+			$cm = 2;
+			}
+		if ($drain > 80 && $drain <= 90)
+		{
+			$cm = 1;
+			}
+		if ($drain > 90 && $drain <= 100)
+		{
+			$cm = 0;
+			}
+		echo "<p>".$cm."</p>";
+		echo "<a href='consequenceMeter.html'>Back</a>";
+	}
+?>
--- ConsequenceMeter/Plawan/login.php
+++ ConsequenceMeter/Plawan/login.php
@@ -5,7 +5,7 @@ if (!$_SESSION['Uid'] )
 	{
 		$e = $_POST['Email'];
 		$p = $_POST['Password'];
-		$query = sprintf("SELECT Uid FROM Users where Email = '%s' AND Password = '%s' LIMIT 1;", $e, $p);
+		$query = sprintf("SELECT * FROM Users where Email = '%s' AND Password = '%s' LIMIT 1;", $e, $p);
 		$result = mysql_query($query);
 		if(mysql_num_rows($result) != 1)
 		{
@@ -19,7 +19,10 @@ if (!$_SESSION['Uid'] )
 				$_SESSION['Uid'] = $row['Uid'];
 				$_SESSION['Email'] = $e;
 				$_SESSION['Name'] = $row['Name'];
-				show_userbox();
+				$_SESSION['RecExpense'] = $row['RecExpense'];
+				$_SESSION['Income'] = $row['Income'];
+				//show_userbox();
+				include "consequenceMeter.html";
 				}
 		}
 		else
@@ -32,5 +35,6 @@ else
 {
 	//user already logged in
 	show_userbox();
+	
 	}
 ?>
